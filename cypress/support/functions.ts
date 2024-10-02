93e6e021337f7ec51df04da7d8edf4df24f2f17e
Pavol Plocica <pali.plocica@gmail.com>
2024-10-02 12:29:59 +0200

    change accept cookies - tryout

--- cypress/support/functions.ts
+++ cypress/support/functions.ts
@@ -1,9 +1,20 @@
 import { searchBox } from './page_objects/search_bar';
 
+// export function acceptCookies(): void {
+//   cy.get('button')
+//     .contains('Accept All', { timeout: 10000 })
+//     .click({ force: true });
+// }
+
 export function acceptCookies(): void {
-  cy.get('button')
-    .contains('Accept All', { timeout: 10000 })
-    .click({ force: true });
+  cy.get('body').then(($body) => {
+    // Check if the button containing 'Accept All' exists
+    if ($body.find('button:contains("Accept All")').length > 0) {
+      // Click the button if it exists
+      cy.get('button').contains('Accept All').click({ force: true });
+    }
+    // If the button does not exist, the test will continue without failing
+  });
 }
 
 export function clickSearchBox(): void {
