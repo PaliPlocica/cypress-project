ce81c2e41303eebe4b4ac20dd8414a5e69ba67d2
Pavol Plocica <pali.plocica@gmail.com>
2024-10-02 11:48:04 +0200

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 2, // Retries in CI
+    openMode: 0, // No retries locally
+  },
   e2e: {
     baseUrl: 'https://docs.cypress.io/',
   },
