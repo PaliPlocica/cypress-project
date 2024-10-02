7625e0148915a2745ad379b6aaca17a026e0838f
Pavol Plocica <pali.plocica@gmail.com>
2024-10-02 14:11:19 +0200

    update cookies

--- cypress/support/functions.ts
+++ cypress/support/functions.ts
@@ -1,23 +1,9 @@
 import { searchBox } from './page_objects/search_bar';
 
-// export function acceptCookies(): void {
-//   cy.get('.osano-cm-accept-all', { timeout: 5000 }).then(($button) => {
-//     if ($button.length > 0) {
-//       cy.wrap($button).contains('Accept All').click({ force: true });
-//     }
-//   });
-// }
-
 export function acceptCookies(): void {
-  // Attempt to get the button, but if it doesn't exist, don't fail
-  cy.get('button:contains("Accept All")', { timeout: 10000 }).then(
-    ($button) => {
-      if ($button.length > 0) {
-        // If the button exists, click it
-        cy.wrap($button).click({ force: true });
-      }
-    },
-  );
+  cy.get('button', { timeout: 10000 })
+    .contains('Accept All')
+    .click({ force: true });
 }
 
 export function clickSearchBox(): void {
